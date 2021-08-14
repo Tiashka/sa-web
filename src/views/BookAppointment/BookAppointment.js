87dd92459aa728935ef22221546197b61525d526
Tiashka <seetaltiashka@gmail.com>
2021-08-14 13:01:21 +0200

    added the second div

--- src/views/BookAppointment/BookAppointment.js
+++ src/views/BookAppointment/BookAppointment.js
@@ -6,7 +6,7 @@ export default class BookAppointment extends Component {
 	render() {
 
 		const formContainerBackground = {
-			width: "50%",
+			width: "40%",
 			height: "90%",
 			backgroundColor: "white",
 			borderRadius: 5,
@@ -14,7 +14,7 @@ export default class BookAppointment extends Component {
 			marginLeft: "10%",
 			marginRight: "10%",
 			justifyContent: "center",
-			textAlign: "center"
+			textAlign: "center",
 		};
 
 		const textFieldContainer = {
@@ -30,34 +30,59 @@ export default class BookAppointment extends Component {
 
 		return (
 			<Box className="book-appointment" display="flex" flexDirection="column" flex="1" alignItems="center" justifyContent="center">
-				<div style={formContainerBackground}>
-					<p style={{ fontSize: 25 }}>Personal Information</p>
-					<div style={formContainer}>
-						<form noValidate autoComplete="off">
-							<div style={textFieldContainer}>
-								<TextField id="outlined-basic" label="Name" variant="outlined" style={{ height: 40, width: "85%", }} />
-							</div>
-							<br />
-							<div style={textFieldContainer}>
-								<TextField id="outlined-basic" label="Surname" variant="outlined" style={{ height: 40, width: "85%", }} />
-							</div>
-							<br />
-							<div style={textFieldContainer}>
-								<TextField id="outlined-basic" label="Email" variant="outlined" style={{ height: 40, width: "85%", }} />
-							</div>
-							<div style={{ display: "flex", justifyContent: "space-between", marginRight: "10%", marginLeft: "10%", paddingTop: "8%" }}>
-								<p>Are you already part of our family?</p>
-								<Checkbox
-									value="checkedA"
-									color="primary"
-									inputProps={{ "aria-label": "Checkbox A" }}
-								/>
-							</div>
-						</form>
+				<div style={{ display: "flex", width: "100%", height: "100%" }}>
+					<div style={formContainerBackground}>
+						<p style={{ fontSize: 25 }}>Personal Information</p>
+						<div style={formContainer}>
+							<form noValidate autoComplete="off">
+								<div style={textFieldContainer}>
+									<TextField id="outlined-basic" label="Name" variant="outlined" style={{ height: 40, width: "85%", }} />
+								</div>
+								<br />
+								<div style={textFieldContainer}>
+									<TextField id="outlined-basic" label="Surname" variant="outlined" style={{ height: 40, width: "85%", }} />
+								</div>
+								<br />
+								<div style={textFieldContainer}>
+									<TextField id="outlined-basic" label="Email" variant="outlined" style={{ height: 40, width: "85%", }} />
+								</div>
+								<div style={{ display: "flex", justifyContent: "space-between", marginRight: "10%", marginLeft: "10%", paddingTop: "8%" }}>
+									<p>Are you already part of our family?</p>
+									<Checkbox
+										value="checkedA"
+										color="primary"
+										inputProps={{ "aria-label": "Checkbox A" }}
+									/>
+								</div>
+							</form>
+						</div>
 					</div>
-				</div>
-				<div style={formContainer}>
-
+					<div style={formContainerBackground}>
+						<p style={{ fontSize: 25 }}>Personal Information</p>
+						<div style={formContainer}>
+							<form noValidate autoComplete="off">
+								<div style={textFieldContainer}>
+									<TextField id="outlined-basic" label="Name" variant="outlined" style={{ height: 40, width: "85%", }} />
+								</div>
+								<br />
+								<div style={textFieldContainer}>
+									<TextField id="outlined-basic" label="Surname" variant="outlined" style={{ height: 40, width: "85%", }} />
+								</div>
+								<br />
+								<div style={textFieldContainer}>
+									<TextField id="outlined-basic" label="Email" variant="outlined" style={{ height: 40, width: "85%", }} />
+								</div>
+								<div style={{ display: "flex", justifyContent: "space-between", marginRight: "10%", marginLeft: "10%", paddingTop: "8%" }}>
+									<p>Are you already part of our family?</p>
+									<Checkbox
+										value="checkedA"
+										color="primary"
+										inputProps={{ "aria-label": "Checkbox A" }}
+									/>
+								</div>
+							</form>
+						</div>
+					</div>
 				</div>
 			</Box>
 		);
