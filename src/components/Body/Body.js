b85b850f5a0035042a94651cb0cbfdc2578de4a1
Tiashka <seetaltiashka@gmail.com>
2021-08-14 16:02:23 +0200

    added the route

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,9 +3,10 @@ import {
 	Switch,
 	Route
 } from "react-router-dom";
-import { BOOK_APPOINTMENT, LANDING } from "constants/routes";
+import { BOOK_APPOINTMENT, CONFIRMATION, LANDING } from "constants/routes";
 import BookAppointment from "views/BookAppointment/BookAppointment";
 import Landing from "views/Landing/Landing";
+import confirmation from "views/BookAppointment/confirmation";
 import "./Body.css";
 
 export default class Body extends Component {
@@ -16,6 +17,9 @@ export default class Body extends Component {
 				<Switch>
 					<Route path={BOOK_APPOINTMENT}>
 						<BookAppointment />
+					</Route>
+					<Route path={CONFIRMATION}>
+						<confirmation />
 					</Route>
 					<Route path={LANDING}>
 						<Landing />
