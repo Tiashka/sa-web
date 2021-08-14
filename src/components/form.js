e15c330ce8e2893040f601da7dbb6c71ac0c6e5d
Tiashka <seetaltiashka@gmail.com>
2021-08-14 11:23:23 +0200

    added form.js

---
+++ src/components/form.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header, Body, Footer } from "components";
+import "../App.css";
+
+function form() {
+    return (
+        <div className="app col flex-grow">
+
+        </div>
+    );
+}
+
+export default form;
